17e918002b821ed51d775e3a150d90780f20297d
Maciel Escudero Bombonato <maciel.bombonato@grupopan.com>
2018-09-18 19:11:16 -0300

    Para o caso de https, adicionando insecure no download do slave.jar

--- tools/run-jenkins-agent.sh
+++ tools/run-jenkins-agent.sh
@@ -29,10 +29,11 @@ export JENKINS_AGENT_WORKDIR="/opt/app"
 #export JENKINS_AGENT_NAME="..."
 
 # Download do conector do agente
-curl ${JENKINS_HOST}/jnlpJars/slave.jar -o "${JENKINS_AGENT_HOME}/${JENKINS_JAR_NAME}" --insecure -noCertificateCheck
+curl ${JENKINS_HOST}/jnlpJars/slave.jar -o "${JENKINS_AGENT_HOME}/${JENKINS_JAR_NAME}" --insecure
 
 # Execucao do agente
 java -jar ${JENKINS_AGENT_HOME}/${JENKINS_JAR_NAME} \
     -jnlpUrl ${JENKINS_HOST}/computer/${JENKINS_AGENT_NAME}/slave-agent.jnlp \
     -secret ${JENKINS_SECRET} \
-    -workDir ${JENKINS_AGENT_WORKDIR}
+    -workDir ${JENKINS_AGENT_WORKDIR} \
+    -noCertificateCheck
